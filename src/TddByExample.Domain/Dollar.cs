868f2936a241d6664fd2703632b00edf9a99ba24
M-okhovvat <Mehdi@DESKTOP-Q9JNDQL>
2020-12-28 01:01:21 +0330

    Add dollar class and tests for it

---
+++ src/TddByExample.Domain/Dollar.cs
@@ -0,0 +1,16 @@
+﻿namespace TddByExample.Domain
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+        }
+
+        public int Amount { get; set; } = 10;
+
+        public void Times(int multiplier)
+        {
+
+        }
+    }
+}
---
+++ test/TddByExample.Tests.Domain/CurrencyTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using TddByExample.Domain;
+using Xunit;
+
+namespace TddByExample.Tests.Domain
+{
+    public class CurrencyTests
+    {
+        [Fact]
+        public void five_dollars_multiplied_by_two_equals_ten_dollars()
+        {
+            var dollar = new Dollar(5);
+
+            dollar.Times(2);
+
+            dollar.Amount.Should().Be(10);
+        }
+    }
+}
