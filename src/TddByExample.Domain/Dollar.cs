b9daae5b02329d5c1feeedc7b247c911b7f4f961
M-okhovvat <Mehdi@DESKTOP-Q9JNDQL>
2020-12-29 00:20:41 +0330

    Return dollar in the times method

--- src/TddByExample.Domain/Dollar.cs
+++ src/TddByExample.Domain/Dollar.cs
@@ -11,8 +11,7 @@
 
         public Dollar Times(int multiplier)
         {
-            Amount *= Amount * multiplier;
-            return null;
+            return new Dollar(Amount * multiplier);
         }
     }
 }
