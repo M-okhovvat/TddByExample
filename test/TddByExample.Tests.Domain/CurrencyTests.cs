6622f9fc02bd2c699b4886ac182efe9996616f34
M-okhovvat <Mehdi@DESKTOP-Q9JNDQL>
2021-01-03 01:15:43 +0330

    Add Franc tests

--- test/TddByExample.Tests.Domain/CurrencyTests.cs
+++ test/TddByExample.Tests.Domain/CurrencyTests.cs
@@ -28,6 +28,84 @@ namespace TddByExample.Tests.Domain
 
             sixDollar.Should().NotBe(anotherSixDollar);
         }
-   
+
+        [Fact]
+        public void define_five_franc()
+        {
+            Franc fiveFranc = new Franc(6);
+
+            fiveFranc.Amount.Should().Be(6);
+        }
+
+        [Fact]
+        public void multiply_franc()
+        {
+            var fiveFranc = new Franc(5);
+
+            Franc tenFranc = fiveFranc.Times(2);
+
+            tenFranc.Amount.Should().Be(10);
+
+            Franc fifteenFranc = fiveFranc.Times(3);
+
+            fifteenFranc.Amount.Should().Be(15);
+
+        }
+
+        [Fact]
+        public void two_franc_with_same_value_are_equal()
+        {
+            var fiveFranc = new Franc(5);
+            var anotherFiveFranc = new Franc(5);
+
+            fiveFranc.Should().Be(anotherFiveFranc);
+
+            var tenFranc = new Franc(10);
+            var anotherTenFranc = new Franc(10);
+
+            tenFranc.Should().Be(anotherTenFranc);
+        }
+
+        [Fact]
+        public void two_franc_with_different_values_are_not_equal()
+        {
+            var fiveFranc = new Franc(5);
+            var eightFranc = new Franc(8);
+
+            fiveFranc.Should().NotBe(eightFranc);
+
+        }
+
+        [Fact]
+        public void null_is_not_equal_to_franc()
+        {
+           
+            var eightFranc = new Franc(8);
+
+            eightFranc.Should().NotBe(null);
+
+        }
+
+
+        public class Franc
+        {
+            public Franc(int amount)
+            {
+                Amount = amount;
+            }
+
+            public int Amount { get; set; }
+
+            public Franc Times(int multipliedBy)
+            {
+                return new Franc(multipliedBy * Amount);
+            }
+            
+            public override bool Equals(object obj)
+            {
+                return ((Franc)obj).Amount == Amount;
+            }
+        }
     }
+
 }
