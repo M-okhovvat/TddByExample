41b96f808411f971041235470e30e4e83f49bd3d
M-okhovvat <Mehdi@DESKTOP-Q9JNDQL>
2021-01-03 23:40:48 +0330

    Fix remove partial from the class decleration

--- test/TddByExample.Tests.Domain/CurrencyTests.cs
+++ test/TddByExample.Tests.Domain/CurrencyTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace TddByExample.Tests.Domain
 {
-    public partial class CurrencyTests
+    public class CurrencyTests
     {
         [Fact]
         public void five_dollar_multiplied_multiple_times()
@@ -32,7 +32,7 @@ namespace TddByExample.Tests.Domain
         [Fact]
         public void define_five_franc()
         {
-            Franc fiveFranc = new Franc(6);
+            TddByExample.Domain.CurrencyTests.Franc fiveFranc = new TddByExample.Domain.CurrencyTests.Franc(6);
 
             fiveFranc.Amount.Should().Be(6);
         }
@@ -40,28 +40,27 @@ namespace TddByExample.Tests.Domain
         [Fact]
         public void multiply_franc()
         {
-            var fiveFranc = new Franc(5);
+            var fiveFranc = new TddByExample.Domain.CurrencyTests.Franc(5);
 
-            Franc tenFranc = fiveFranc.Times(2);
+            TddByExample.Domain.CurrencyTests.Franc tenFranc = fiveFranc.Times(2);
 
             tenFranc.Amount.Should().Be(10);
 
-            Franc fifteenFranc = fiveFranc.Times(3);
+            TddByExample.Domain.CurrencyTests.Franc fifteenFranc = fiveFranc.Times(3);
 
             fifteenFranc.Amount.Should().Be(15);
-
         }
 
         [Fact]
         public void two_franc_with_same_value_are_equal()
         {
-            var fiveFranc = new Franc(5);
-            var anotherFiveFranc = new Franc(5);
+            var fiveFranc = new TddByExample.Domain.CurrencyTests.Franc(5);
+            var anotherFiveFranc = new TddByExample.Domain.CurrencyTests.Franc(5);
 
             fiveFranc.Should().Be(anotherFiveFranc);
 
-            var tenFranc = new Franc(10);
-            var anotherTenFranc = new Franc(10);
+            var tenFranc = new TddByExample.Domain.CurrencyTests.Franc(10);
+            var anotherTenFranc = new TddByExample.Domain.CurrencyTests.Franc(10);
 
             tenFranc.Should().Be(anotherTenFranc);
         }
@@ -69,22 +68,18 @@ namespace TddByExample.Tests.Domain
         [Fact]
         public void two_franc_with_different_values_are_not_equal()
         {
-            var fiveFranc = new Franc(5);
-            var eightFranc = new Franc(8);
+            var fiveFranc = new TddByExample.Domain.CurrencyTests.Franc(5);
+            var eightFranc = new TddByExample.Domain.CurrencyTests.Franc(8);
 
             fiveFranc.Should().NotBe(eightFranc);
-
         }
 
         [Fact]
         public void null_is_not_equal_to_franc()
         {
-           
-            var eightFranc = new Franc(8);
+            var eightFranc = new TddByExample.Domain.CurrencyTests.Franc(8);
 
             eightFranc.Should().NotBe(null);
-
         }
     }
-
 }
